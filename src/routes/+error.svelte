2d38808b0f908f0071472352686f1f7f4e31db94
Alex Sieke <asieke@gmail.com>
2023-03-27 14:10:31 -0400

    error 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<!-- src/routes/_error.svelte -->
+<script lang="ts">
+	export let status: number;
+	export let error: { message: string };
+</script>
+
+<main>
+	<h1>Error {status}: {error.message}</h1>
+	<p>Sorry, the page you requested could not be found.</p>
+</main>
