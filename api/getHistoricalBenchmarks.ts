7c1ea981d6cb4aaa652cb176481006cb74263a59
Alex Sieke <asieke@gmail.com>
2023-06-08 15:45:22 -0400

    edge function

--- api/getHistoricalBenchmarks.ts
+++
@@ -1,8 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export default (req: VercelRequest, res: VercelResponse): void => {
-	res.status(200).send('Hello, World!');
-};
