55bd5ea60e47cc7c179ce51349b0bfbab18a7b0b
Alex Sieke <asieke@gmail.com>
2023-06-15 09:45:05 -0400

    supabase client added to serverless

--- api/assets/updateAllocation.ts
+++ api/assets/updateAllocation.ts
@@ -1,6 +1,10 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function updateAllocation(request: VercelRequest, response: VercelResponse) {
+import supabase from '../supabaseClient.ts';
+
+export default async function updateAllocation(request: VercelRequest, response: VercelResponse) {
+	console.log(supabase);
+
 	// 1. Reject if not a POST request
 	if (request.method !== 'POST') {
 		response.status(405).send('Method Not Allowed'); // 405 is the status code for Method Not Allowed
@@ -11,18 +15,25 @@ export default function updateAllocation(request: VercelRequest, response: Verce
 	const incomingKey = process.env.INCOMING_KEY;
 	const requestApiKey = request.headers['api_key']; // Adjusted to accommodate your curl command
 
-	console.log('incomingKey', incomingKey);
-	console.log('requestApiKey', requestApiKey);
-
 	if (requestApiKey !== incomingKey) {
 		response.status(401).send('Unauthorized'); // 401 is the status code for Unauthorized
 		return;
 	}
 
+	// get the number of rows in supabase transactions table
+	const { data, error } = await supabase.from('asset_classes').select('*');
+	if (error) {
+		response.status(500).send('Internal Server Error');
+		return;
+	}
+
+	console.log('HELLO THERE!!!', data);
+
 	// Continue processing...
 	response.status(200).json({
 		body: request.body,
 		query: request.query,
-		cookies: request.cookies
+		cookies: request.cookies,
+		data: JSON.stringify(data)
 	});
 }
