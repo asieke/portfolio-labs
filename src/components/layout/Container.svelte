abec1540dc2f21d3653212001482400f9eec096e
Alex Sieke <asieke@gmail.com>
2023-07-19 17:31:32 -0400

    built auth UI scaffolding

--- src/components/layout/Container.svelte
+++ src/components/layout/Container.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
-	export let layout: 'left' | 'right' | 'even' | 'full' = 'full';
+	type Layout = 'left' | 'right' | 'even' | 'full' | 'tight';
+
+	export let layout: Layout = 'full';
 
 	let gridClass = `grid w-full grid-cols-1 gap-4 md:grid-cols-2`;
 	if (layout === 'left') gridClass = `grid w-full grid-cols-1 gap-4 md:grid-cols-leftcol`;
 	if (layout === 'right') gridClass = `grid w-full grid-cols-1 gap-4 md:grid-cols-rightcol`;
 </script>
 
-{#if layout === 'full'}
+{#if layout === 'tight'}
+	<div class="mx-auto w-fit rounded-xl bg-slate-50 p-8 shadow-lg dark:bg-dark-900">
+		<slot />
+	</div>
+{:else if layout === 'full'}
 	<div class="w-full rounded-xl bg-slate-50 p-8 shadow-lg dark:bg-dark-900">
 		<slot />
 	</div>
---
+++ src/routes/auth/signin/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { Container } from '$components/layout';
+
+	let email = 'asieke@gmail.com';
+
+	const handleSubmit = () => {
+		console.log('time to log in => ', email);
+	};
+</script>
+
+<Container layout="tight">
+	<div class="w-[50vw] md:w-[400px]">
+		<h3 class="text-center">Sign in to your account</h3>
+
+		<div class="mt-5">
+			<label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+			<div class="mt-2">
+				<input
+					bind:value={email}
+					id="email"
+					name="email"
+					type="email"
+					autocomplete="email"
+					required
+					class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+				/>
+			</div>
+		</div>
+
+		<button class="submit mt-6 w-full" on:click={handleSubmit}>Sign In</button>
+		<p class="mt-10 text-center text-sm text-gray-500">
+			Not a member?
+			<a href="/" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+		</p>
+	</div>
+</Container>
