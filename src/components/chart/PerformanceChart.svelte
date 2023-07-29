a1135af8e05ce7b9d7a72f673534047daf5ed112
Alex Sieke <asieke@gmail.com>
2023-07-29 16:30:44 -0400

    refactored chart to create a chart library

--- src/components/chart/PerformanceChart.svelte
+++ src/components/chart/PerformanceChart.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { color, formatPercent } from '$lib/utils/format';
 	import { getCAGR } from '$lib/utils/financial';
-	import Highcharts from 'highcharts/highstock';
+	import { drawChart } from '$lib/chart/lineChart';
 	import type { Balance } from '$types/balances';
 	import { chartDates, chartSelectedDate, chartSelectedBenchmarks, balanceDisplayData } from '$lib/stores/performanceChart';
 	import DatePicker from './DatePicker.svelte';
@@ -11,12 +11,6 @@
 
 	// Define the prop for the component
 	export let balances: Balance[];
-	export let options: {
-		datePicker?: boolean;
-		benchmarkPicker?: boolean;
-		benchmarkTable?: boolean;
-	} = {};
-	const { datePicker = true, benchmarkPicker = true, benchmarkTable = true } = options;
 
 	const defaultBenchmark = 'US Equity Total Market';
 
@@ -34,7 +28,7 @@
 
 	$: benchmarkBalances = [...displayBalances].filter((b) => b.date <= '2099-01-01');
 
-	const onChartMouseMove = (x: number) => {
+	const onMouseOver = (x: number) => {
 		const date = new Date(x).toISOString().split('T')[0];
 		benchmarkBalances = [...displayBalances].filter((b) => b.date <= date);
 		if (benchmarkBalances.length === 0) {
@@ -42,7 +36,7 @@
 		}
 	};
 
-	const onChartMouseOut = () => {
+	const onMouseOut = () => {
 		benchmarkBalances = [...displayBalances];
 	};
 
@@ -76,171 +70,24 @@
 		data = temp;
 
 		if (container) {
-			drawChart();
+			drawChart({ container, data, onMouseOver, onMouseOut, showY: false });
 		}
 	}
 
-	// Initialize container and chart variables
 	let container: HTMLElement;
-	let chart: Highcharts.Chart | null = null;
-
-	// Define function for getting chart data
-
-	// Define function for drawing the chart
-	const drawChart = () => {
-		chart = Highcharts.stockChart(container, {
-			chart: {
-				backgroundColor: undefined,
-				plotBorderWidth: undefined
-			},
-			accessibility: {
-				enabled: false
-			},
-			rangeSelector: {
-				enabled: false
-			},
-			title: {},
-			series: data,
-			credits: {
-				enabled: false
-			},
-			xAxis: {
-				labels: {
-					style: {
-						color: '#aaa',
-						fontSize: '12px'
-					}
-				},
-				lineColor: '#707073',
-				tickColor: '#707073',
-				crosshair: {
-					color: '#ccc',
-					width: 1,
-					dashStyle: 'Dash' as Highcharts.DashStyleValue
-				}
-			},
-			yAxis: {
-				labels: {
-					style: {
-						color: '#aaa'
-					},
-					formatter: function () {
-						return '$' + this.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-					}
-				}
-			},
-			tooltip: {
-				positioner: function (labelWidth, labelHeight, point) {
-					if (point.plotX < labelWidth / 2) {
-						return { x: 0, y: 5 };
-					} else if (point.plotX > this.chart.chartWidth - 10 - labelWidth / 2) {
-						return { x: this.chart.chartWidth - labelWidth - 10, y: 5 };
-					}
-
-					return { x: point.plotX - labelWidth / 2, y: 5 };
-				},
-				enabled: true,
-				formatter: function () {
-					// Format the date using Highcharts.dateFormat function
-					if (this.x === undefined || typeof this.x !== 'number') {
-						return 'Error: Invalid date';
-					}
-
-					//calculate how many pixels away the y value is from the top of the chart
-					let width = this.point.series.chart.plotWidth || (0 as number);
-					let x = this.point.plotX || (0 as number);
-					let y = this.point.plotY || (0 as number);
-
-					// let x = this.point.plotX < 54 ? this.point.plotX + 4 : 54;
-
-					let left = 0;
-					if (x < 52) left = x + 2;
-					else if (width - x < 46) left = 92 - (width - x);
-					else left = 52;
-
-					let top = y - 10;
-
-					return `
-            <div class='z-2 w-[100px] text-center px-3 py-2 shadow rounded-full text-xs dark:bg-slate-600 bg-white text-slate-500 dark:text-slate-300'>
-              ${Highcharts.dateFormat('%b %e, %Y', this.x)}
-            </div>
-            <div class="z-1 absolute rounded-full bg-blue-600 w-3 h-3" style="top: ${top}px; left: ${left}px"></div>
-            `;
-				},
-				backgroundColor: undefined,
-				borderWidth: 0,
-				borderColor: '#ccc',
-				shadow: false,
-				borderRadius: 0,
-				padding: 0,
-				distance: 0,
-				useHTML: true,
-				shape: 'rect' as Highcharts.TooltipShapeValue,
-
-				style: {
-					// CSS properties for the text in the tooltip box
-					color: '#333333',
-					fontSize: '11px'
-				}
-			},
-			plotOptions: {
-				series: {
-					animation: {
-						duration: 0,
-						easing: 'easeInOutSine'
-					},
-					events: {
-						mouseOut: () => {
-							onChartMouseOut();
-						}
-					},
-					states: {
-						hover: {
-							enabled: false
-						},
-						inactive: {
-							opacity: 1
-						}
-					},
-					point: {
-						events: {
-							mouseOver: function () {
-								onChartMouseMove(this.x);
-							}
-						}
-					}
-				}
-			}
-		});
-	};
 </script>
 
 <!-- Display the selected date -->
 <section class="mb-6">
 	<h4>My Total Performance: <span class={color(performancePct)}>{formatPercent(performancePct)}</span></h4>
-	<p>
-		CAGR: {formatPercent(getCAGR(performancePct, $chartSelectedDate))}
-	</p>
-
+	<p>CAGR: {formatPercent(getCAGR(performancePct, $chartSelectedDate))}</p>
 	<p>
 		US Total Equity Benchmark: <span class={color(benchmarkPct)}>{formatPercent(benchmarkPct)}</span>
 		<span class="text-sm">({formatPercent(Math.abs(performancePct / benchmarkPct - 1))} {performancePct > benchmarkPct ? 'higher' : 'lower'})</span>
 	</p>
 </section>
 
-<!-- Include date picker and benchmark picker components, redraw chart on date change -->
-{#if datePicker}
-	<DatePicker />
-{/if}
-
-<!-- Container for the Highcharts chart -->
+<DatePicker />
 <div class="h-[440px]" bind:this={container} />
-
-<!-- Include benchmark picker component -->
-{#if benchmarkPicker}
-	<BenchmarkPicker />
-{/if}
-
-{#if benchmarkTable}
-	<BenchmarkTable balances={displayBalances.length - benchmarkBalances.length >= 2 ? benchmarkBalances : displayBalances} />
-{/if}
+<BenchmarkPicker />
+<BenchmarkTable balances={displayBalances.length - benchmarkBalances.length >= 2 ? benchmarkBalances : displayBalances} />
---
+++ src/lib/chart/lineChart.ts
@@ -0,0 +1,139 @@
+import Highcharts from 'highcharts/highstock';
+import type { SeriesLineOptions } from 'highcharts';
+import { formatCurrency } from '$lib/utils/format';
+
+type ChartProps = {
+	data: SeriesLineOptions[];
+	onMouseOver?: (x: number) => void;
+	onMouseOut?: () => void;
+	container: HTMLElement;
+	showX?: boolean;
+	showY?: boolean;
+};
+
+export const drawChart = ({ container, data, onMouseOver, onMouseOut, showX = true, showY = true }: ChartProps) => {
+	// Create a Highcharts stock chart in the given container with the specified configuration
+
+	Highcharts.stockChart(container, {
+		chart: {
+			backgroundColor: undefined,
+			plotBorderWidth: undefined
+		},
+		accessibility: {
+			enabled: false
+		},
+		rangeSelector: {
+			enabled: false
+		},
+		title: {},
+		series: data,
+		credits: {
+			enabled: false
+		},
+		xAxis: {
+			labels: {
+				style: {
+					color: '#aaa',
+					fontSize: '12px'
+				}
+			},
+			lineColor: '#707073',
+			tickColor: '#707073',
+			crosshair: {
+				color: '#ccc',
+				width: 2,
+				dashStyle: 'Dash' as Highcharts.DashStyleValue
+			}
+		},
+		yAxis: {
+			labels: {
+				style: {
+					color: '#aaa'
+				},
+				formatter: function () {
+					// Format y-axis labels as dollar amounts with comma separators
+					return '$' + this.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+				}
+			}
+		},
+		tooltip: {
+			positioner: function (labelWidth, labelHeight, point) {
+				if (point.plotX < 50) {
+					return { x: 0, y: 0 };
+				}
+
+				if (point.plotX > this.chart.chartWidth - 70) {
+					return { x: this.chart.chartWidth - 120, y: 0 };
+				}
+
+				return { x: point.plotX - 50, y: 0 };
+			},
+			enabled: true,
+			formatter: function () {
+				// Check if the x value of the data point is valid
+				if (this.x === undefined || typeof this.x !== 'number') {
+					return 'Error: Invalid date';
+				}
+
+				// Calculate tooltip position
+				const chartWidth = this.point.series.chart.plotWidth || 0;
+				const dataPointX = this.point.plotX || 0;
+				const dataPointY = this.point.plotY || 0;
+
+				let left = 55;
+				if (dataPointX < 50) left = dataPointX + 5;
+				if (dataPointX > chartWidth - 50) left = 105 - (chartWidth - dataPointX);
+
+				const top = dataPointY - 8;
+
+				console.log(dataPointX, dataPointY, chartWidth);
+
+				return `
+          <div class='z-10 opacity-80 absolute top-[5px] left-[10px] w-[100px] rounded-xl text-center px-3 py-2 text-xs dark:bg-slate-600 bg-slate-200 text-slate-500 dark:text-slate-300'>
+            ${showX ? Highcharts.dateFormat('%b %e, %Y', this.x) : ''}
+						${showY ? `<div>${formatCurrency(this.y)}</div>` : ''}
+          </div>
+					<div class='z-5 absolute w-[10px] h-[10px] rounded-full bg-blue-500' style='left: ${left}px; top: ${top}px'>
+
+        `;
+			},
+			backgroundColor: undefined,
+			borderWidth: 0,
+			borderColor: undefined,
+			borderRadius: 0,
+			padding: 0,
+			distance: 0,
+			useHTML: true
+		},
+		plotOptions: {
+			series: {
+				animation: {
+					duration: 0,
+					easing: 'easeInOutSine'
+				},
+				events: {
+					// Trigger the onMouseOut function when the mouse leaves the chart area
+					mouseOut: () => {
+						if (onMouseOut) onMouseOut();
+					}
+				},
+				states: {
+					hover: {
+						enabled: false
+					},
+					inactive: {
+						opacity: 1
+					}
+				},
+				point: {
+					events: {
+						// Trigger the onMouseOver function when the mouse hovers over a data point
+						mouseOver: function () {
+							if (onMouseOver) onMouseOver(this.x);
+						}
+					}
+				}
+			}
+		}
+	});
+};
