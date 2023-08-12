107752bc5a35b91287dbf7936c033da1fe1d7a2e
Alex Sieke <asieke@gmail.com>
2023-08-12 06:13:50 -0400

    removed console log

--- src/routes/(app)/modeling/+page.svelte
+++ src/routes/(app)/modeling/+page.svelte
@@ -2,9 +2,9 @@
 	import { Container } from '$components/layout';
 
 	export let data;
-	console.log(data);
 </script>
 
 <Container>
 	<h3>Modeling Impact</h3>
+	<span>There are {data.benchmarkPrices.length} items</span>
 </Container>
