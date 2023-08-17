caf021d6754c45a4649a3436456452ade6ee4c1b
Alex Sieke <asieke@gmail.com>
2023-08-16 21:32:12 -0400

    updating AI contexts and adding page switching logic (#15)
    * updating AI contexts and adding page switching logic
    
    * removing console log

--- src/lib/stores/ai.ts
+++ src/lib/stores/ai.ts
@@ -1,7 +1,18 @@
 import { writable } from 'svelte/store';
 
-export const context = writable('');
+export const context = writable<{ page: string; content: string }>({ page: '', content: '' });
+export const chatMessages = writable<{ role: string; content: string }[]>([]);
 
-export const addAIContext = (content: string) => {
-	context.update(() => content);
+export const addAIContext = (object: { page: string; content: string }) => {
+	context.set(object);
 };
+
+context.subscribe((value) => {
+	if (value.page === '') return;
+	chatMessages.update((messages) => {
+		if (messages[messages.length - 1] && messages[messages.length - 1].content.substring(0, 18) === 'Switching to page:') {
+			return [...messages.slice(0, -1), { role: 'assistant', content: 'Switching to page: ' + value.page }];
+		}
+		return [...messages, { role: 'assistant', content: 'Switching to page: ' + value.page }];
+	});
+});
